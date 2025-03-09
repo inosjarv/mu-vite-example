b1ea90e7f141139cebb6cbf59536da47f3d2962d
Vraj Soni <vrajds8725@gmail.com>
2025-03-09 18:18:30 +0530

    reverted vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,14 +8,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     cors: true,
-    configureServer: (server) => {
-      server.middlewares.use((req, res, next) => {
-        res.setHeader('Set-Cookie', `cookie=${process.env.COOKIE}; Path=/; HttpOnly`)
-        next()
-      })
-    },
   },
-
   build: {
     outDir: '../src/main/resources/static/',
     emptyOutDir: true,
